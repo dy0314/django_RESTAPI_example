e8915ef5dcf37f1c1f52d7f3171b44b56d7b45fd
d_yong.lee <dy0314@gmail.com>
2020-02-22 10:01:38 +0900

    Finish

--- django_restapi/api_user/models.py
+++ django_restapi/api_user/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
 # Create your models here.
 
 class User(models.Model):
-    user_id = models.CharField(max_length=100, null=False)
-    password = models.CharField(max_length=100, null=False)
+    user_id = models.CharField(max_length=128, null=False)
+    password = models.CharField(max_length=128, null=False)
+    address = models.CharField(max_length=256, null=True)
+
+    class Meta:
+        db_table = "User" #Table이름을 "User"로 정한다 default 이름은 api_user_user가 된다.
